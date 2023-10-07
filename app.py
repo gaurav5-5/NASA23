98949f9254f336028424de497adc66b4a097f01c
gaurav5-5 <72967185+gaurav5-5@users.noreply.github.com>
2023-10-07 18:49:38 +0000

    feedback, app.py

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 # Sqlite3 code
 FEEDBACK = 'data/feedback.db'
+REPORT = 'data/report.db'
 
-
-def get_db():
+def get_db(db_path):
     db = getattr(g, '_database', None)
     if db is None:
-        db = g._database = sqlite3.connect(FEEDBACK)
+        db = g._database = sqlite3.connect(db_path)
     return db
 
 
@@ -22,12 +22,12 @@ def close_connection(exception):
         db.close()
 
 
-def query_db(query, args=(), one=False, commit=False):
-    cur = get_db().execute(query, args)
+def query_db(db_path,query, args=(), one=False, commit=False):
+    cur = get_db(db_path).execute(query, args)
     rv = cur.fetchall()
     cur.close()
     if commit:
-        get_db().commit()
+        get_db(db_path).commit()
     return (rv[0] if rv else None) if one else rv
 
 
@@ -41,18 +41,42 @@ def index():
 @app.route('/feedback', methods=['GET', 'POST'])
 def feedback():
     if request.method == 'POST':
-        na = request.form.get('fd-name')
-        em = request.form.get('fd-email')
-        msg = request.form.get('fd-message')
-        print(na,em,msg)
+        if 'rprt-name' in request.form:
+            na = request.form.get('rprt-name')
+            em = request.form.get('rprt-email')
+            msg = request.form.get('rprt-message')
+            print(na,em,msg)
 
-        # check if table exists
-        query_db(
-            "CREATE TABLE IF NOT EXISTS feedback (name TEXT, email TEXT, feedback TEXT)")
+            # check if table exists
+            query_db(REPORT,
+                "CREATE TABLE IF NOT EXISTS feedback (name TEXT, email TEXT, feedback TEXT)")
 
-        # Insert data into database
-        query_db("INSERT INTO feedback (name, email, feedback) VALUES (?, ?, ?)",
-                 (na, em, msg), commit=True)
+            # Insert data into database
+            query_db(REPORT,
+                     "INSERT INTO feedback (name, email, feedback) VALUES (?, ?, ?)",
+                     (na, em, msg), commit=True)
+            
+            # flash('Reported Successfully!', category='success')
+
+            # return render_template('feedback_success.html', title='Contact Us!')
+        
+        else:
+            na = request.form.get('fd-name')
+            em = request.form.get('fd-email')
+            msg = request.form.get('fd-message')
+            print(na,em,msg)
+
+            # check if table exists
+            query_db(FEEDBACK,
+                "CREATE TABLE IF NOT EXISTS feedback (name TEXT, email TEXT, feedback TEXT)")
+
+            # Insert data into database
+            query_db(FEEDBACK,
+                    "INSERT INTO feedback (name, email, feedback) VALUES (?, ?, ?)",
+                    (na, em, msg), commit=True)
+            
+            # flash('Thank you for your feedback!', category='success')
+            
 
         return render_template('feedback_success.html', title='Contact Us!')
     return render_template('feedback.html', title='Feedback')
