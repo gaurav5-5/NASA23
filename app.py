93f4e7aaacfe0c8691f14c464cabe256cde64a12
gaurav5-5 <72967185+gaurav5-5@users.noreply.github.com>
2023-10-07 13:12:34 +0000

    updted app.py

--- app.py
+++ app.py
@@ -1,7 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+
+
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form.get('nav'):
+            return render_template('feedback.html', title='Contact Us!')
+    else:
+        return render_template('index.html', title='Fire Forecasters')
+
+@app.route('/feedback', methods=['GET', 'POST'])
+def feedback():
+    return render_template('feedback.html', title='Feedback')
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/protocol', methods=['GET', 'POST'])
+def protocol():
+    return render_template('protocol.html', title='Protocol')
+
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
