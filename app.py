16a73cc4d998fe83cbc5f15d030d455bb9da3bce
gaurav5-5 <72967185+gaurav5-5@users.noreply.github.com>
2023-10-08 17:42:50 +0000

    d

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import model as model
 import sqlite3
 from flask import Flask, render_template, request, g, jsonify
 import pandas as pd
+import numpy    as np
 
 import geopandas as gpd
 from shapely.geometry import Point, Polygon
@@ -58,7 +59,66 @@ def update_db(req, table=None):
 
 
 
+# data visualization
+def pie_areas():
+    
+    cntr = ["ITA", "ARG", "SPA", "USA", "IND"]
+    cntrn = ["Italy", "Argentina", "Spain", "USA", "India"]
+    areas = []
+    for c in cntr:
+        df = pd.read_csv(f'https://firms.modaps.eosdis.nasa.gov/api/country/csv/{MAP_KEY}/VIIRS_SNPP_NRT/{c}/1/')
+        areas.append(calculate_damaged_area_coordinates(df['latitude'], df['longitude']))
 
+    plt.pie(np.array(areas), labels=cntrn)
+    plt.savefig(f'static/images/area.png')
+
+
+
+def pie_frp():
+    
+    countries = ["ITA", "ARG", "USA", "IND", "ESP"]
+    countries_n = ["Italy", "Argentina", "United States of America", "India", "Spain"]
+    MAP_KEY = 'c4f9c87a128b458b6a69d96cfc2e3e60'
+
+    frp = []
+
+    for c in countries:
+
+        df = pd.read_csv(f'https://firms.modaps.eosdis.nasa.gov/api/country/csv/{MAP_KEY}/VIIRS_SNPP_NRT/{c}/1/')
+        col = df['frp']
+        cm = col.mean()
+        cm = f"{cm:.3f}"
+        cm = float(cm)
+        frp.append(cm)
+
+        y = np.array(frp)
+        print(y)
+
+        # Create the Pie Chart
+        plt.pie(y, labels=countries_n, autopct='%1.1f%%', startangle=140)
+
+        # Customize the Pie Chart
+        plt.title('FRP')
+        plt.axis('equal')
+        # plt.legend()
+
+        # Display the Chart
+        plt.show()
+        plt.savefig("{{ url_for('static', '/images/frp.png') }}", transparent=True)
+
+
+
+def pie_bti4():
+    
+    cntr = ["ITA", "ARG", "SPA", "USA", "IND"]
+    cntrn = ["Italy", "Argentina", "Spain", "USA", "India"]
+    bti4 = []
+    for c in cntr:
+        df = pd.read_csv(f'https://firms.modaps.eosdis.nasa.gov/api/country/csv/{MAP_KEY}/VIIRS_SNPP_NRT/{c}/1/')
+        bti4.append(df['bright_ti4'].mean())
+
+    plt.pie(np.array(bti4), labels=cntrn)
+    plt.savefig(f'static/images/bti4.png')
 
 
 # data fetching"]
@@ -84,22 +144,11 @@ def calculate_damaged_area_coordinates(latitude, longitude):
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
-    cntrn = ["Italy", "Argentina", "Spain", "USA", "India"]
-    cntr = ["ITA", "ARG", "SPA", "USA", "IND"]
-
-    frp = []
-    area = []
-    bti4 = []
-
-    for c in cntr:
-        url = f"https://firms.modaps.eosdis.nasa.gov/api/country/csv/{MAP_KEY}/VIIRS_SNPP_NRT/{c}/1/"
-        data = pd.read_csv(url)
-        frp.append(data['frp'].mean())
-        calc = calculate_damaged_area_coordinates(data['latitude'], data['longitude'])
-        area.append(calc)
-        bti4.append(data['bright_ti4'].mean())
+    pie_areas()
+    pie_bti4()
+    pie_frp()
     
-    return render_template('index.html', title='Fire Forecasters', frp=frp, area=area, bti4=bti4, countries=cntrn)
+    return render_template('index.html', title='Fire Forecasters')
 
 
 
